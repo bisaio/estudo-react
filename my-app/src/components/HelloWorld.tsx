c0896e54efcfc5e6ddd7a8e1afcc6813a21aa034
Guilherme Bisaio <m322998@alunos.ipb.pt>
2026-02-04 22:11:57 +0000

    aula_4 - criacao de componentes

---
+++ my-app/src/components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import Frase from "./Frase"
+
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+            <Frase/>
+        </div>
+    )
+} 
