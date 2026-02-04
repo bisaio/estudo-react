d1c39eaffc9c7cb512e6684387908657f479bb2e
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-04 21:42:26 +0000

    aula_3 - entendendo JSX

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,22 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name: string = 'Zé';
+  const formatName: string = name.toUpperCase();
+
+  const sum = (a: number, b: number): number => a + b
+
+  const url = "https://placehold.co/150"
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fala, {formatName}</h1>
+      <h2>Soma: {sum(3,5)}</h2>
+      <img src={url} alt='placeholder'/>
     </div>
   );
 }
