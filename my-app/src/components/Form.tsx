ce9ab1c1b9729899988b7e3d09758d66d5850037
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-05 15:10:07 +0000

    aula_10 - useState

--- my-app/src/components/Form.tsx
+++ my-app/src/components/Form.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function Form() {
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    
     function cadastrarUsuario(e: React.SyntheticEvent) { // e = event
         e.preventDefault(); // para a exec do formulario (comportamento padrao do HTML) e faz o log
-        console.log("usuario cadastrado");   
+        console.log(name);
+        console.log(`usuario: ${name} | senha: ${password}`);
     }
 
     return (
@@ -9,7 +15,12 @@ export default function Form() {
             <h1>Cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label htmlFor="name" style={{marginRight: 5}}>Nome</label>
+                    <input id="name" name="name" type="text" placeholder="Digite seu nome" onChange={(e) => {setName(e.target.value)}}/>
+                </div>
+                <div>
+                    <label htmlFor="password" style={{marginRight: 5}}>Senha</label>
+                    <input id="password" name="password" type="password" placeholder="Digite sua senha" onChange={(e) => {setPassword(e.target.value)}}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
