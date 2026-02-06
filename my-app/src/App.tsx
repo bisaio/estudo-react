337e86308e8e9a72a5b135dcc96a036efd5835b8
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-06 15:51:43 +0000

    aula_13 - render de lisas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import RenderizacaoCondicional from './components/RenderizacaoCondicional';
+import RenderList from './components/RenderList';
 
 function App() {
 
+  const lista = ['React', 'Vue', 'Angular']
+
   return (
     <div className="App">
-      <h1>Renderização Condicioanl</h1>
-      <RenderizacaoCondicional/>
+      <h1>Renderização de Listas</h1>
+      <RenderList itens={lista}/>
+      <RenderList itens={[]}/>
     </div>
   );
 }
