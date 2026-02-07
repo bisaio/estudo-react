3c883436f93fa7a757cc7ea60bf92eb8e6872f8d
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-07 20:09:54 +0000

    aula_16 - react-icons

--- my-app/src/components/layout/Footer.tsx
+++ my-app/src/components/layout/Footer.tsx
@@ -1,3 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 export default function Footer() {
-    return <footer>Rodapé</footer>
+    const FacebookIcon = FaFacebook as any;
+    const InstagramIcon = FaInstagram as any;
+    const LinkedinIcon = FaLinkedin as any;
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FacebookIcon /></li>
+                <li><InstagramIcon /></li>
+                <li><LinkedinIcon /></li>
+            </ul>
+        </footer>
+    )
 }
